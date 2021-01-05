a8a8ab68d70bca2d97e621f1da79173a8476cfdd
Craig_Donavin <crdonavin@gmail.com>
2021-01-05 09:44:32 -0600

    add stringChopper instance to click method

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,22 @@
 class App {
     constructor() {
         this.initBindingsAndEventListeners()
+        
     }
     
     initBindingsAndEventListeners() {
         this.mainContainer = document.getElementById('main-container')
         this.btn = this.mainContainer.querySelector('#choppin-button')
         this.input = this.mainContainer.querySelector("#search-term-input")
+        this.queryTable = this.mainContainer.querySelector(".query-table")
         this.btn.addEventListener('click', this.handleClick.bind(this))
     }
 
     handleClick(e) {
         let input = this.input.value
-        console.log(input)
-        
+        if (input) this.stringChopper = new StringChopper(input)
+        console.log(this.stringChopper.input)
     }
+
+
 }
