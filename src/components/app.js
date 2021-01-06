b1a167d76f146c8d1142b19b3df92b2593495cd4
Craig_Donavin <crdonavin@gmail.com>
2021-01-06 00:25:44 -0600

    create table builder

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,7 @@ class App {
         this.queryTable = this.mainContainer.querySelector(".query-table")
         this.btn.addEventListener('click', this.handleClick.bind(this))
         this.checkboxes = document.getElementById('checkboxes')
+        this.tbody = document.getElementById('tbody')
     }
 
 
@@ -22,6 +23,19 @@ class App {
         let queryObj = this.stringChopper.queryObj
         console.log(this.stringChopper.input)
         console.log(this.objectBuilder(checkboxes, this.stringChopper.input, queryObj))
+        for (let key in queryObj) {
+            console.log(`${key}: ${queryObj[key]}`)
+        }
+        this.tableBuilder(this.objectBuilder(checkboxes, input, queryObj))
+    }
+
+    tableBuilder(queryObj){
+        for (let key in queryObj) {
+            let row = document.createElement('tr')
+            row.setAttribute('id', `${key}`)
+            row.innerHTML = `${key}`
+            this.tbody.appendChild(row)
+        }
     }
 
     objectBuilder(inputs, string, queryObj){
@@ -31,16 +45,16 @@ class App {
         let source
         let chopperKey
         let chunkerKey
-        for (let i=0; i<5; i+=2) {
+        for (let i=0; i<6; i+=2) {
             if (inputs[i].checked) {
                 chopperKey = `chopper${inputs[i].dataset.value}` 
                 chunkerKey = `chunker${inputs[i].dataset.value}`
                 source = { [chopperKey] : this.stringChopper.chopper(string, parseInt(inputs[i].dataset.value)),
-                    [chunkerKey] : this.stringChopper.chunker(string, inputs[i].dataset.value)}
+                    [chunkerKey] : this.stringChopper.chunker(string, parseInt(inputs[i].dataset.value))}
                 Object.assign(queryObj, source)
             }
-            return queryObj
         }
+        return queryObj
     }
 
 
